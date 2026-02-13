39fa72dc89e47717001082da9c21c3519075c94d
LeeSanderson <lee.sanderson@codurance.com>
2026-02-13 16:01:02 +0000

    Data update

--- run.ps1
+++ run.ps1
@@ -32,7 +32,7 @@ $RaceDataPath = Resolve-Path ".\Data"
 try {
     Invoke-NativeCommand dotnet build
     Invoke-NativeCommand dotnet test
-    Invoke-NativeCommand $RaceDownloaderExe updateresults --output $RaceDataPath --period 308
+    Invoke-NativeCommand $RaceDownloaderExe updateresults --output $RaceDataPath --period 336
     ## Invoke-NativeCommand $RaceDownloaderExe deduperesults --output $RaceDataPath
     # Invoke-NativeCommand $RaceDownloaderExe updateresults --output $RaceDataPath --period 365
     # Invoke-NativeCommand $RaceDownloaderExe validate --output $RaceDataPath
