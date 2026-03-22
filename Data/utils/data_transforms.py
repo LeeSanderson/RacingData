085382b6812f96de0f136d8d4d78af213db92b35
LeeSanderson <lee.sanderson@codurance.com>
2026-03-22 11:16:22 +0000

    Data update

--- Data/utils/data_transforms.py
+++ Data/utils/data_transforms.py
@@ -2,20 +2,35 @@ import pandas as pd
 
 surface_categories = ["Surface_AllWeather", "Surface_Dirt", "Surface_Turf"]
 
-def encode_surfaces(races : pd.DataFrame) -> pd.DataFrame:
-    races = races.drop(surface_categories + ["Surface_Unknown"], axis=1, errors='ignore')
+
+def encode_surfaces(races: pd.DataFrame) -> pd.DataFrame:
+    races = races.drop(
+        surface_categories + ["Surface_Unknown"], axis=1, errors="ignore"
+    )
     races["SurfaceTemp"] = races["Surface"]
-    races = pd.get_dummies(races, prefix="Surface", columns=["SurfaceTemp"], dtype=float)
-    races = races.drop("Surface_Unknown", axis=1) # Drop unknown surface as only small number.
+    races = pd.get_dummies(
+        races, prefix="Surface", columns=["SurfaceTemp"], dtype=float
+    )
+    races = races.drop(
+        "Surface_Unknown", axis=1
+    )  # Drop unknown surface as only small number.
     return races
 
 
-going_categories  = ["Going_Good", "Going_Good_To_Soft", "Going_Soft", "Going_Good_To_Firm", "Going_Firm", "Going_Heavy"]
+going_categories = [
+    "Going_Good",
+    "Going_Good_To_Soft",
+    "Going_Soft",
+    "Going_Good_To_Firm",
+    "Going_Firm",
+    "Going_Heavy",
+]
 
-def encode_going(races : pd.DataFrame) -> pd.DataFrame:
+
+def encode_going(races: pd.DataFrame) -> pd.DataFrame:
     # Normalise going based on rules here: https://www.racingpost.com/guide-to-racing/what-is-the-going-ann7h6W6VB3b/
     # Values should be: Firm, Good_To_Firm, Good, Good_To_Soft, Soft, Heavy
-    norm_map = ({
+    norm_map = {
         "Good": "Good",
         "Standard": "Good",
         "Soft": "Soft",
@@ -33,9 +48,25 @@ def encode_going(races : pd.DataFrame) -> pd.DataFrame:
         "Slow": "Soft",
         "Sloppy": "Heavy",
         "Muddy": "Heavy",
-        "Frozen": "Heavy"
-    })
+        "Frozen": "Heavy",
+    }
 
     races["NormGoing"] = races["Going"].map(norm_map)
-    races = races.drop(going_categories, axis=1, errors='ignore')
+    races = races.drop(going_categories, axis=1, errors="ignore")
     return pd.get_dummies(races, prefix="Going", columns=["NormGoing"], dtype=float)
+
+
+race_type_categories = [
+    "RaceType_Other",
+    "RaceType_Hurdle",
+    "RaceType_SteepleChase",
+    "RaceType_Flat",
+]
+
+
+def encode_race_type(races: pd.DataFrame) -> pd.DataFrame:
+    races = races.drop(race_type_categories, axis=1, errors="ignore")
+    races["RaceTypeTemp"] = races["RaceType"]
+    return pd.get_dummies(
+        races, prefix="RaceType", columns=["RaceTypeTemp"], dtype=float
+    )
